eeff0184218b646efbcbfbcb2abcc65fc5b7336e
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-23 01:24:37 +0900

    update config

--- lib/ex_sitemap_generator/config.ex
+++ lib/ex_sitemap_generator/config.ex
@@ -1,10 +1,10 @@
 defmodule ExSitemapGenerator.Config do
 
   defstruct [
-    max_sitemap_files:    50_000,      # max sitemap links per index file
-    max_sitemap_links:    50_000,      # max links per sitemap
-    max_sitemap_news:     1_000,       # max news sitemap per index_file
-    max_sitemap_images:   1_000,       # max images per url
+    max_sitemap_files:    45_000,      # max sitemap links per index file
+    max_sitemap_links:    45_000,      # max links per sitemap
+    max_sitemap_news:     950,         # max news sitemap per index_file
+    max_sitemap_images:   950,         # max images per url
     max_sitemap_filesize: 10_000_000,  # bytes
   ]
 
@@ -12,10 +12,10 @@ defmodule ExSitemapGenerator.Config do
   def configure(overwrite) do
     ow = overwrite
     start_link(%__MODULE__{
-      max_sitemap_files:    ow[:max_sitemap_files]    || System.get_env("SITEMAP_MAXFILES")    || Application.get_env(:ex_sitemap_generator, :max_sitemap_files,    50_000),
-      max_sitemap_links:    ow[:max_sitemap_links]    || System.get_env("SITEMAP_MAX_LINKS")   || Application.get_env(:ex_sitemap_generator, :max_sitemap_links,    50_000),
-      max_sitemap_news:     ow[:max_sitemap_news]     || System.get_env("SITEMAP_MAXNEWS")     || Application.get_env(:ex_sitemap_generator, :max_sitemap_news,     1_000),
-      max_sitemap_images:   ow[:max_sitemap_images]   || System.get_env("SITEMAP_MAXIMAGES")   || Application.get_env(:ex_sitemap_generator, :max_sitemap_images,   1_000),
+      max_sitemap_files:    ow[:max_sitemap_files]    || System.get_env("SITEMAP_MAXFILES")    || Application.get_env(:ex_sitemap_generator, :max_sitemap_files,    45_000),
+      max_sitemap_links:    ow[:max_sitemap_links]    || System.get_env("SITEMAP_MAX_LINKS")   || Application.get_env(:ex_sitemap_generator, :max_sitemap_links,    45_000),
+      max_sitemap_news:     ow[:max_sitemap_news]     || System.get_env("SITEMAP_MAXNEWS")     || Application.get_env(:ex_sitemap_generator, :max_sitemap_news,     950),
+      max_sitemap_images:   ow[:max_sitemap_images]   || System.get_env("SITEMAP_MAXIMAGES")   || Application.get_env(:ex_sitemap_generator, :max_sitemap_images,   950),
       max_sitemap_filesize: ow[:max_sitemap_filesize] || System.get_env("SITEMAP_MAXFILESIZE") || Application.get_env(:ex_sitemap_generator, :max_sitemap_filesize, 10_000_000),
     })
   end
