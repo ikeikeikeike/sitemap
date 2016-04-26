8490a1ed7513ad052527def175fbe2d3a08795d5
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-27 01:21:52 +0900

    rename

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Sitemap.Mixfile do
    [
      app: :sitemap,
      name: "Sitemap",
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: ">= 1.0.0",
      description: @description,
      build_embedded: Mix.env == :prod,
