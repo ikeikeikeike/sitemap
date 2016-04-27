99321388642f38adcf8b4e68708d80a789a2303a
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-04-27 17:31:15 +0900

    pkg to be on application module

--- lib/sitemap.ex
+++ lib/sitemap.ex
@@ -1,17 +1,48 @@
 defmodule Sitemap do
   use Application
-  alias Sitemap.Config
+  # alias Sitemap.Config
+
+  # def start_link, do: start(nil, [])
+  # def start(_type, _args) do
+  #   Config.configure
+  #   cfg = Config.get
+
+  #   Sitemap.Builders.File.init
+  #   Sitemap.Builders.Indexfile.init
+  #   Sitemap.Location.init(:file, filename: cfg.filename, zero: 1, start: 2)
+  #   Sitemap.Location.init(:indexfile, filename: cfg.filename)
+  # end
+
+  # @doc false
+  # defmacro __using__(_opts) do
+  #   quote do
+  #     use Sitemap.DSL
+  #   end
+  # end
+
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    Sitemap.Config.configure
+    cfg = Sitemap.Config.get
+
+    children = [
+      worker(Sitemap.Builders.File, []),
+      worker(Sitemap.Builders.Indexfile, []),
+      worker(Sitemap.Location, [:file, [filename: cfg.filename, zero: 1, start: 2]], id: :file),
+      worker(Sitemap.Location, [:indexfile, [filename: cfg.filename]], id: :indexfile),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Sitemap.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 
   def start_link, do: start(nil, [])
-  def start(_type, _args) do
-    Config.configure
-    cfg = Config.get
-
-    Sitemap.Builders.File.init
-    Sitemap.Builders.Indexfile.init
-    Sitemap.Location.init(:file, filename: cfg.filename, zero: 1, start: 2)
-    Sitemap.Location.init(:indexfile, filename: cfg.filename)
-  end
 
   @doc false
   defmacro __using__(_opts) do
@@ -19,5 +50,7 @@ defmodule Sitemap do
       use Sitemap.DSL
     end
   end
+
+
 
 end
--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Sitemap.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [], included_applications: [:xml_builder], mod: {Sitemap, []}]
   end
 
   # Dependencies can be Hex packages:
