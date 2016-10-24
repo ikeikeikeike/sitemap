d80e1f9bc34dbfbf235ffdef83d7dca667b4d0d7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-10-24 14:19:33 +0900

    travis and bumps update

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Sitemap.Mixfile do
    [
      app: :sitemap,
      name: "Sitemap",
-     version: "0.7.1",
+     version: "0.7.2",
      elixir: ">= 1.0.0",
      description: @description,
      build_embedded: Mix.env == :prod,
